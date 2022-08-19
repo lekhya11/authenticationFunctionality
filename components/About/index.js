715f99f699153d8ce76ca1c770bc88422a27f82f
k.lekhya <107404763+lekhya11@users.noreply.github.com>
2022-08-19 19:29:02 +0530

    Create index.js

---
+++ components/About/index.js
@@ -0,0 +1,15 @@
+import Header from '../Header'
+import LogoutButton from '../LogoutButton'
+import './index.css'
+
+const About = () => (
+  <>
+    <Header />
+    <div className="About-container">
+      <h1 className="heading">About Route</h1>
+      <LogoutButton />
+    </div>
+  </>
+)
+
+export default About
